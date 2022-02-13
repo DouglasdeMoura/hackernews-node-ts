d90d798f8a3e0cf6cda3d0c18f3f311242c5c787
Douglas Moura <douglas.ademoura@gmail.com>
2022-02-13 15:29:40 -0300

    feat: add Link mutation

--- src/schema.ts
+++ src/schema.ts
@@ -20,13 +20,27 @@ const resolvers = {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-
   Link: {
     id: (parent: Link) => parent.id,
     description: (parent: Link) => parent.description,
     url: (parent: Link) => parent.url,
-  }
-}
+  },
+  Mutation: {
+    post: (parent: unknown, args: { description: string, url: string }) => {
+      let idCount = links.length;
+
+      const link: Link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+
+      return link;
+    },
+  },
+};
 
 export const schema = makeExecutableSchema({
   typeDefs,
