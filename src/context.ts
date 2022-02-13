44b5c285766107c8da383d0865ec30c90e753a20
Douglas Moura <douglas.ademoura@gmail.com>
2022-02-13 15:51:07 -0300

    feat: add Prisma client to the GraphQL context

--- src/context.ts
+++ src/context.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export type GraphQLContext = {
+  prisma: PrismaClient;
+};
+
 export async function contextFactory() {
-  return {};
+  return {
+    prisma,
+  };
 }
