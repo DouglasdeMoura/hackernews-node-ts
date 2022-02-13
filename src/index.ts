a53c3a72e622fdf5b6ec8dca1429241c06994658
Douglas Moura <douglas.ademoura@gmail.com>
2022-02-13 15:13:08 -0300

    chore: create first schema and get results programatically

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World')
+import 'graphql-import-node';
+import { execute, parse } from "graphql";
+import { schema } from "./schema";
+
+async function main() {
+  const myQuery = parse(`query { info }`);
+
+  const result = await execute({
+    schema,
+    document: myQuery,
+  });
+
+  console.log(result);
+}
+
+main();
